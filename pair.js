89832e410f48bb72081aaedbc3aaf42545128e5d
Pesadu <pesadukalan321@gmail.com>
2025-05-30 02:50:54 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://raw.githubusercontent.com/Pesadu/BOT-HELPER/refs/heads/main/6118299098223333127_121.jpg",
               },
               caption: sid,
             });
